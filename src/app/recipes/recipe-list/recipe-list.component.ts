157967a5a15d2abb858308e4d8e913d9d942d540
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2023-05-31 19:10:03 +0700

    Add recipe model. Use the model in recipe-list and recipe-detail components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a simple test recipe', 'https://tinyurl.com/2p2sc9z7'),
+  ];
 }
