2c21e8ea060a427035036742a8553e90f56a5006
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2023-05-31 17:32:58 +0700

    Add header, recipes and shopping list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShopiingEditComponent } from './shopping-list/shopiing-edit/shopiing-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShopiingEditComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping-list/shopiing-edit/shopiing-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopiing-edit',
+  templateUrl: './shopiing-edit.component.html',
+  styleUrls: ['./shopiing-edit.component.scss']
+})
+export class ShopiingEditComponent {
+
+}
