3d59c94e92987d6402584ff4aea9591272b64054
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2023-06-01 20:42:31 +0700

    Correct a type in component name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HeaderComponent} from "./header/header.component";
+import { HeaderComponent } from "./header/header.component";
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShopiingEditComponent } from './shopping-list/shopiing-edit/shopiing-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,7 @@ import { ShopiingEditComponent } from './shopping-list/shopiing-edit/shopiing-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShopiingEditComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule
