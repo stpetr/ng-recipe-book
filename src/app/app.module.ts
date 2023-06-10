3437fc4d8a259ace92af2aec03ada19c69a4562f
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2023-06-10 20:42:51 +0700

    Create auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,17 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from './app.component';
-import {AuthComponent} from './auth/auth.component';
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {HeaderComponent} from "./header/header.component";
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,6 +24,7 @@ import {SharedModule} from "./shared/shared.module";
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
+    AuthModule,
     RecipesModule,
     ShoppingListModule,
   ],
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+
+import {SharedModule} from "../shared/shared.module";
+import {AuthComponent} from "./auth.component";
+import {FormsModule} from "@angular/forms";
+
+const authRoutes: Routes = [
+  { path: 'auth', component: AuthComponent },
+];
+
+@NgModule({
+  declarations: [
+    AuthComponent,
+  ],
+  imports: [
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild(authRoutes),
+  ],
+})
+export class AuthModule {}
